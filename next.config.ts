bcd013cd8f41883b9af272a2c5ad06188e60b94a
Zhaii43 <hanzprahinog@gmail.com>
2025-05-30 11:14:15 +0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "https://backend-r9v8.onrender.com",
-        port: "8000",
+        protocol: "https",
+        hostname: "backend-r9v8.onrender.com",
         pathname: "/media/**",
       },
     ],
